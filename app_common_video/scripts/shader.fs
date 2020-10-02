b105428b32c1e2b2e185e37694217d64bde2784e
LucaRitz <ritz.luca@gmail.com>
2020-10-02 09:49:43 +0200

    Fixed ambient

--- app_common_video/scripts/shader.fs
+++ app_common_video/scripts/shader.fs
@@ -8,7 +8,7 @@ in vec3 Normal;
 uniform sampler2D texture1;
 
 void main() {
-	float ambientFactor = 0.2;
+	float ambientFactor = 1;
 	vec3 ambientLight = ambientFactor * vec3(1.0, 1.0, 1.0);
 	FragColor = vec4(ambientLight, 1.0) * texture(texture1, TexCoord);
 }
